1e5e59e5d54f031852554e3783f0b7b572279093
Gustavo Neves <129140834+Sevengusta@users.noreply.github.com>
2023-05-09 20:41:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+// jogo da memória
+keys = ["q","w","e","a","s","d","z","x","c","v","b","n"]
+// retornar um item aleatório do array keys
+function randomizer (){
+    let index = Math.floor(Math.random() *keys.length);
+    return keys[index];
+}
 document.body.addEventListener('keyup', (event) => {
     playSound(event.code.toLowerCase())
 
 })
-document.querySelector('.composer button').addEventListener('click', (createSong) => {
+document.querySelector('.composer button.guess').addEventListener('click', () => {
     let song = document.querySelector('#input').value;
 
     if (song !== ''){
         let songArray = song.split('');
         playComposition(songArray);
+        };
     }
-
-
-})
-
-
+)
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`)
     
@@ -38,6 +42,67 @@ function playComposition(songArray){
 
         setTimeout(() => {
             playSound(`key${songItem}`);
+            
+        }, wait)
+
+        wait += 350;
+    }
+}
+
+// composição fácil
+let easy = document.querySelector('.composer button.easy').addEventListener('click', () => {
+     playEasyComposition(randomizer() + randomizer() + randomizer());
+})
+function playEasyComposition(easy){
+    let wait = 0;
+    for(let easy of songArray){
+
+        setTimeout(() => {
+            playSound(`key${easy}`);
+            
+        }, wait)
+
+        wait += 350;
+    }
+}
+function playEasyComposition(songArray){
+    let wait = 0;
+    for(let songItem of songArray){
+
+        setTimeout(() => {
+            playSound(`key${songItem}`);
+            
+        }, wait)
+
+        wait += 350;
+    }
+}
+// composição normal
+document.querySelector('.composer button.normal').addEventListener('click', () => {
+    playComposition(randomizer() + randomizer() + randomizer() + randomizer() + randomizer());
+})
+function playNormalComposition(normal){
+    let wait = 0;
+    for(let normal of songArray){
+
+        setTimeout(() => {
+            playSound(`key${normal}`);
+            
+        }, wait)
+
+        wait += 350;
+    }
+}
+// composição normal
+document.querySelector('.composer button.hard').addEventListener('click', () => {
+    playComposition(randomizer() + randomizer() + randomizer() + randomizer() + randomizer() + randomizer() + randomizer() );
+})
+function playHardComposition(hard){
+    let wait = 0;
+    for(let hard of songArray){
+
+        setTimeout(() => {
+            playSound(`key${hard}`);
             
         }, wait)
 
